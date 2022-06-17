8139e2580fa013afd3e114dc5c0be78abe4cf1b7
Francisco Mendonça <francisco.jose.oliveira05@aluno.ifce.edu.br>
2022-06-17 07:31:35 -0300

    teste dao.

--- src/sistema_academico/SistemaAcademico.java
+++ src/sistema_academico/SistemaAcademico.java
@@ -1,80 +1,39 @@
 
 package sistema_academico;
-/*
-import java.util.Scanner;
-import Model.Aluno;
-import Model.Professor;
-*/
 
-import dao.DB;
-import java.sql.Connection;
-import java.sql.Statement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+
+import Model.Professor;
+import Modeo.dao.FabricaDAO;
+import Modeo.dao.ProfessorDAO;
+import java.util.List;
+//import java.util.List;
 
 /**
  * @author Evandro
  */
 public class SistemaAcademico {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        /*
-        Scanner Obj = new Scanner(System.in);
         
-        //Entradas de texto
-        System.out.println("Digite o nome: ");
-        String nome = Obj.nextLine();
+        ProfessorDAO professorDao = FabricaDAO.criarProfessorDao();
         
-        System.out.println("Digite o email: ");
-        String email = Obj.nextLine();
+        System.out.println("=====  Teste 1: Professor Insert =====");
+	Professor professor = new Professor(12398754, false, "Artul", "artul@email", "12377733");
+        professorDao.insert(professor);
         
-        System.out.println("Digite a senha: ");
-        String senha = Obj.nextLine();
+        System.out.println("=====  Teste 2: Professor Delete =====");
+        professorDao.deletePorNumR(12398754);
         
-        //Entradas numericas
-        System.out.println("Digite o Matricula: ");
-        Integer matricula = Obj.nextInt();
-        
-        System.out.println("Digite se é um adimistrador");
-        boolean boo = Obj.nextBoolean();
-        
-        Aluno aluno01 = new Aluno(matricula, nome, email, senha);
-        System.out.println(aluno01.getNome() + " " + aluno01.getEmail() + " " + aluno01.getSenha());
-        
-        Professor professor = new Professor(matricula, boo, nome, email, senha);
-        System.out.println(professor.getNome() + " " + professor.getEmail() + " "
-            + professor.getSenha() + " " + professor.getAdiministrador());
-        
-        Obj.close();
-        */
-        
-        Connection conn = null;
-	Statement st = null;
-	ResultSet rs = null;
-		
-	try {
-            conn = DB.getConnection();           // Abrindo uma conecção.
-            st = conn.createStatement();  // Instanciei um objeto o "st".
-			
-            rs = st.executeQuery("select * from professor");
-			
-            while (rs.next()) {
-                System.out.println(rs.getInt(1) + ", " + rs.getString(2)    
-                + ", " + rs.getString(3) + ", " + rs.getString(4)+ ", "
-                + rs.getBoolean(5));
-            }
-	}
-	catch (SQLException e) {
-            e.printStackTrace();
-		}
-	finally {
-            DB.closeResultSet(rs);	
-            DB.closeStatement(st);
-            DB.closeConnection();
-	}
-    }
+        System.out.println("=====  Teste 3: Professor Select =====");
+        List<Professor> lista = professorDao.listarProf();
+        for (Professor obj : lista) {
+            System.out.println(obj.getNumRegistro() + ", " + obj.getNome() + ", " 
+                + obj.getEmail() + ", " + obj.getSenha() + ", " + obj.getAdiministrador()); 
+        }
 
+        System.out.println("=====  Teste 4: Professor Update =====");
+        Professor professor01 = new Professor(12398754, false, "Lara", "lara@Gemail.com", "33377711");
+        professorDao.update(professor);
+        
+    }
 }
